35ea90ba0fa1fc78c8450c8d92e8638c0fb8d5d9
Eduardo <eduardo@massolini.com.ar>
2022-10-16 21:31:38 -0300

    Create script.js

---
+++ cac2022/js/integrador/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const frmTickets = document.getElementById('frmTickets');
+    const btnClear = document.getElementById('btnClear');
+    const spanSubTotal = document.getElementById('subTotal');
+
+    btnClear.addEventListener("click", () => { btnClear_click(spanSubTotal) });
+    frmTickets.addEventListener("submit", (e) => { frmTickets_submit(e,spanSubTotal) });
+});
+
+
+const btnClear_click = (spnST) => {
+    document.getElementById("frmTickets").reset();
+    spnST.innerText = '';
+}
+
+const frmTickets_submit  = (e, spnST) => {
+    e.preventDefault();
+    let frmData = new FormData(e.originalTarget);
+    let total = parseInt(frmData.get('amount')) * 200 * (100 - parseInt(frmData.get('category'))) / 100;
+    spnST.innerText = total;
+}
