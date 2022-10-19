1c5e4aeb29efcb88d05cda0d35f5ee8f46d202f2
Eduardo <eduardo@massolini.com.ar>
2022-10-19 17:38:59 -0300

    Create users.js

---
+++ cac2022/js/fetch/js/users.js
@@ -0,0 +1,38 @@
+function Users(userJson) {
+
+    const html = `
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Avatar</th>
+            </tr>
+            </thead>
+            <tbody>
+                ${userJson.map(user=>User(user)).join('')}
+            </tbody>
+        </table>
+    `;
+
+    document.getElementById('users').innerHTML = html;
+
+    //return html;
+}
+
+function User(user) {
+    const html = `
+        <tr>
+            <th scope="row">${user.id}</th>
+            <td>${user.first_name}</td>
+            <td>${user.last_name}</td>
+            <td>${UserImage(user.avatar)}</td>
+        </tr>
+    `;
+    return html;
+}
+
+function UserImage(urlImagePath) {
+    return `<img src="${urlImagePath}">`;
+}
